0e69a17a41e11f956e9353fba96cc97b5552403d
Kazuhiro Kubota <k2.wanko@gmail.com>
2020-05-30 19:10:56 +0900

    Setup firebase

---
+++ app.config.js
@@ -0,0 +1,6 @@
+export default ({ config }) => {
+  // console.log(config.name); // prints 'My App'
+  return {
+    ...config,
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Analytics from 'expo-firebase-analytics';
 
 export default function App(): JSX.Element {
+  if (__DEV__) {
+    Analytics.setDebugModeEnabled(true).catch((err) => {
+      console.warn(err.message);
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>FJUG</Text>
+      <Button
+        title="click"
+        onPress={() => {
+          Analytics.logEvent('click', {
+            test: true,
+          });
+        }}
+      />
     </View>
   );
 }
